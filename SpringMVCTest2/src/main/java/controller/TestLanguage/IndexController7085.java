17ec71233a947e48bb6e170014adb735b2fb6117
15091329896 <2200364978@qq.com>
2020-07-09 09:26:16 +0800

    Java国际化 完全跑通  英文验证失败

---
+++ SpringMVCTest2/src/main/java/controller/TestLanguage/IndexController7085.java
@@ -0,0 +1,23 @@
+package controller.TestLanguage;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * @ClassName: IndexController7082
+ * @Description: 测试JSON交互（请求响应全是JSON）
+ * @author dyk
+ * @date 2020-07-07 16:56:47
+ */
+@Controller
+@RequestMapping("/index7085")
+public class IndexController7085 {
+
+	private static final Log logger = LogFactory.getLog(IndexController7085.class);
+	
+	
+
+}
